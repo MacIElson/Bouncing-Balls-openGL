d4b168a58a0f228e69fe4f545a05b8b5a34d396c
Mac-I Crowell <macicrowell@gmail.com>
2015-11-18 23:44:50 -0500

    CODE IS UGLY BUT WORKS, has a gmae loop which updates and renders at 60fps, ball bounces around

---
+++ addGameLoop/ODLGameLoop_private.h
@@ -0,0 +1,31 @@
+#ifndef ODLGAMELOOP_PRIVATE_H_
+#define ODLGAMELOOP_PRIVATE_H_
+
+typedef struct ODLGameLoopStateStruct {
+	int desiredStateUpdatesPerSecond;
+	int desiredStateUpdateDurationMs;
+
+	int gameEntitiesLength;
+
+	double lastLoopTime;
+	double timeAccumulatedMs;
+
+	int upsCount;
+	int fpsCount;
+	double lastMeasurementTime;
+
+	double simulatedDelayDurationMs;
+} ODLGameLoopState;
+
+extern ODLGameLoopState odlGameLoopState;
+
+// void ODLGameLoop_initOpenGL();
+// void ODLGameLoop_updateState();
+// void ODLGameLoop_onOpenGLIdle();
+// void ODLGameLoop_updateGraphics();
+// void ODLGameLoop_onWindowReshape();
+// void ODLGameLoop_onKeyboard(unsigned char key, int x, int y);
+// void ODLGameLoop_updateMeasurements();
+
+
+#endif /* ODLGAMELOOP_PRIVATE_H_ */
