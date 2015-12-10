c37c2271cd6b6307068dfc9f9898ab5315edb4f1
Mac-I Crowell <macicrowell@gmail.com>
2015-12-10 10:58:49 -0500

    made slight change in how dt is calculated for update, should never be negative

--- newest/components.cpp
+++ newest/components.cpp
@@ -46,6 +46,7 @@ void Component::update( float dt ) {} //will be overrided in a subclass, so we d
 void Component::fixedUpdate( float dt ) {}
 
 void Component::updateAll( float dt ){
+  printf("floatingUpdatedt:%f \n", (float) dt);
   for(Component* elem: Component::components){
     elem->update( dt );
   }
@@ -143,8 +144,8 @@ void ODLGameLoop_onOpenGLDisplay() {
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear Screen
   double now = glutGet(GLUT_ELAPSED_TIME);
-  double dt = odlGameLoopState.lastLoopTime - now;
-  // printf("lastLoop:%f now:%f dt:%f \n", odlGameLoopState.lastLoopTime, now, dt);
+  double dt = now - odlGameLoopState.lastLoopTime;
+  printf("lastLoop:%f now:%f dt:%f \n", odlGameLoopState.lastLoopTime, now, dt);
   
   Component::updateAll(dt);
   odlGameLoopState.fpsCount++;
